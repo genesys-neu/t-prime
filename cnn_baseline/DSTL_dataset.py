7fbe138c3f8fb5243289890f92055fb5b3ff92e9
Mauro Belgiovine <mbelgiovine89@gmail.com>
2023-01-21 22:13:12 -0500

    Added code for cnn baseline

---
+++ cnn_baseline/DSTL_dataset.py
@@ -0,0 +1,192 @@
+import sys
+import scipy.io as sio
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import pickle
+import argparse
+from glob import glob
+import os
+from tqdm import tqdm
+
+class FileCache:
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.cache = {}
+        self.access_queue = []
+
+    def _evict_oldest(self):
+        oldest_file = self.access_queue.pop(0)
+        self.cache.pop(oldest_file)
+
+    def get(self, file_path):
+        if file_path in self.cache:
+            self.access_queue.remove(file_path)
+            self.access_queue.append(file_path)
+            return self.cache[file_path]
+        else:
+            return None
+
+    def put(self, file_path, content):
+        if len(self.cache) == self.max_size:
+            self._evict_oldest()
+        if file_path in self.cache:
+            self.access_queue.remove(file_path)
+        self.cache[file_path] = content
+        self.access_queue.append(file_path)
+
+
+class DSTLDataset(Dataset):
+    def __init__(self,
+                 protocols: list,  # this list will contain the folder/protocols names to include in the dataset
+                 slice_len: int,    # this will define the slice size of each signal that will be input to the model
+                 ds_type='train',  # either 'train' or 'test'
+                 slice_overlap_ratio=0.5,   # this is the overlap ratio for each slice generated from a signal
+                                            # this value will affect the number of slices that is possible to create from each signal
+                 ds_path='/home/mauro/Research/DSTL/DSTL_DATASET_1_0',
+                 noise_model='AWGN', noise_dbm=0,
+                 override_gen_map=False,
+                 normalize=False,
+                 transform=None, target_transform=None):
+
+        self.protocols = protocols
+        self.slice_len = slice_len
+        self.slice_overlap_ratio = slice_overlap_ratio
+
+        assert(ds_type in ['train', 'test'])
+        self.ds_type = ds_type
+
+        info_filename = 'ds_info__'+str(len(protocols))+'class.pkl'
+        ds_info_path = os.path.join(ds_path, info_filename)
+        do_gen_info = True
+        if os.path.exists(ds_info_path) and (not override_gen_map):
+            ans = input('File '+info_filename+' already exists. Do you wanna create a new one? [y/n]')
+            if ans.lower() in ['n', 'no']:
+                do_gen_info = False
+
+        if do_gen_info and (not override_gen_map):
+            self.ds_info = self.generate_ds_map(ds_path, info_filename)
+        else:
+            self.ds_info = pickle.load(open(ds_info_path, 'rb'))
+
+        if normalize:
+            #Todo
+            pass
+
+        # let's assign inputs and labels
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.signal_cache = FileCache(max_size=20e4)
+
+    def generate_ds_map(self, ds_path, filename, test_ratio=0.2):
+        examples_map = {}
+        class_map = dict(zip(self.protocols, range(len(self.protocols))))
+        # retrieve the list of signals (.mat) from every folder/protocol specified
+        for i, p in enumerate(self.protocols):
+            path = os.path.join(ds_path, p)
+            if os.path.isdir(path):
+                mat_list = sorted(glob(os.path.join(path, '*.mat')))
+                examples_map[p] = dict(
+                    zip(
+                        list(range(len(mat_list))),
+                        mat_list
+                    )
+                )
+            else:
+                sys.exit('[DSTLDataset] folder ' + path + ' not found. Aborting...')
+
+        # now let's go through each class examples and assign a global sample index
+        # based on the slice len and overlap configuration
+
+        # also, we need to assign a unique slice index to each possible slice to be used with __getitem__ function
+        # and assign the corresponding label
+        data_ixs = {}
+        labels_ixs = {}
+        ixs_count = 0
+        for c in tqdm(examples_map.keys(), desc='Analyzing signal dataset...'):
+            for ix, path in examples_map[c].items():
+                sig = sio.loadmat(path)
+                len_sig = sig['waveform'].shape[0]
+                overlap = int(self.slice_len * self.slice_overlap_ratio)
+                window_ixs = list(range(0, len_sig-self.slice_len, overlap)) 
+                n_windows = len(window_ixs)
+                examples_map[c][ix] = {'slices': window_ixs,
+                                       'path': examples_map[c][ix]}  # here we modify the original content
+                for w in window_ixs:
+                    data_ixs[ixs_count] = {'path': path, 'slice_ix': w}
+                    labels_ixs[ixs_count] = class_map[c]
+                    ixs_count += 1
+
+
+        # lastly, we separate the training and testing dataset by randomly sampling a certain % of data samples indexes
+        test_data_ixs = {}
+        test_labels_ixs = {}
+        test_ixs = np.random.choice(ixs_count, size=int(ixs_count*test_ratio), replace=False)
+        for i in test_ixs.tolist():
+            i_data = data_ixs.pop(i)
+            test_data_ixs[i] = i_data
+            i_label = labels_ixs.pop(i)
+            test_labels_ixs[i] = i_label
+        # we also need to store a map from linear indexes (used by ray when retrieving data from dataset)
+        train_ixs = dict(zip(range(len(data_ixs.keys())), list(data_ixs.keys())))
+        test_ixs = dict(zip(range(len(test_data_ixs.keys())), list(test_data_ixs.keys())))
+
+
+        # after creating class map and examples map, let's store this information for future use
+        ds_info_path = os.path.join(ds_path, filename)
+        ds_info = {
+            'class_map': class_map, 'examples_map': examples_map,
+            'ds_indexes':
+                {
+                    'train': {'data': data_ixs, 'labels': labels_ixs},
+                    'test': {'data': test_data_ixs, 'labels': test_labels_ixs}
+                },
+            'ixs_maps': {'train': train_ixs, 'test': test_ixs}  # linear indexes maps (used by ray/torch)
+            }
+        pickle.dump(ds_info, open(ds_info_path, 'wb'))
+
+        return ds_info
+
+    def info(self):
+        ds_info = {
+            'slice_len': self.slice_len,
+            'numsamps': {'train' : len(self.ds_info['ds_indexes']['train']['data'].keys()),
+                         'test': len(self.ds_info['ds_indexes']['test']['data'].keys())},
+            'nclasses': len(self.protocols),
+            'nchans': 2,    # real and imag components are separated on different channels
+        }
+        return ds_info
+
+    def __len__(self):
+        return len(self.ds_info['ds_indexes'][self.ds_type]['data'].keys())
+
+    def __getitem__(self, idx):
+        dataset = self.ds_info['ds_indexes'][self.ds_type]
+        # first retrieve the internal sample index relative to the linear index idx
+        s_idx = self.ds_info['ixs_maps'][self.ds_type][idx]
+        # retrieve the info relative to this input sample
+        obs_info = dataset['data'][s_idx]
+        # let's first retrieve the signal from the cache (or load it in if not present)
+        sig = self.signal_cache.get(obs_info['path'])
+        if sig is None:
+            mat_dict = sio.loadmat(obs_info['path'])
+            self.signal_cache.put(obs_info['path'], mat_dict['waveform'])
+            sig = self.signal_cache.get(obs_info['path'])
+        # then we obtain the relative slice
+        obs = sig[obs_info['slice_ix']:obs_info['slice_ix']+self.slice_len, 0]
+        obs = np.stack((obs.real, obs.imag))
+        label = dataset['labels'][s_idx]
+
+        if self.transform:
+            obs = self.transform(obs)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return obs, label
+
+
+if __name__ == "__main__":
+    myds = DSTLDataset(['802_11ax', '802_11b', '802_11n', '802_11g'],slice_len=128, slice_overlap_ratio=0.5)
+    for _ in range(10):
+        index = np.random.choice(list(myds.ds_info['ds_indexes']['train']['data'].keys()))
+        obs, lbl = myds[index]
---
+++ cnn_baseline/DSTL_torch_train.py
@@ -0,0 +1,230 @@
+import numpy as np
+from DSTL_dataset import DSTLDataset
+from ray.air import session, Checkpoint
+from typing import Dict
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from ray.train.torch import TorchTrainer, TorchPredictor
+from ray.air.config import ScalingConfig
+import ray.train as train
+import pickle
+from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present
+
+
+# Define model
+class ConvNN(nn.Module):
+    def __init__(self, numChannels=1, slice_len=4, num_feats=18, classes=3):
+        super(ConvNN, self).__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.numChannels = numChannels
+
+        # initialize first set of CONV => RELU => POOL layers
+        self.conv1 = nn.Conv1d(in_channels=numChannels, out_channels=64, kernel_size=7)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool1d(kernel_size=2, stride=2)
+        ##  initialize second set of CONV => RELU => POOL layers
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128,
+                            kernel_size=3)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool1d(kernel_size=2, stride=2)
+        ## initialize first (and only) set of FC => RELU layers
+
+        # pass a random input
+        rand_x = torch.Tensor(np.random.random((1, numChannels, slice_len)))
+        output_size = torch.flatten(self.maxpool2(self.conv2(self.maxpool1(self.conv1(rand_x))))).shape
+        self.fc1 = nn.Linear(in_features=output_size.numel(), out_features=256)
+        self.relu3 = nn.ReLU()
+        # initialize our softmax classifier
+        self.fc2 = nn.Linear(in_features=256, out_features=classes)
+        self.logSoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        # x = x.reshape((x.shape[0], self.numChannels, x.shape[2]))   # CNN 1D expects a [N, Cin, L] size of data
+        # pass the input through our first set of CONV => RELU =>
+        # POOL layers
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.maxpool1(x)
+        ## pass the output from the previous layer through the second
+        ## set of CONV => RELU => POOL layers
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.maxpool2(x)
+        ## flatten the output from the previous layer and pass it
+        ## through our only set of FC => RELU layers
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        # pass the output to our softmax classifier to get our output
+        # predictions
+        x = self.fc2(x)
+        output = self.logSoftmax(x)
+        # return the output predictions
+        return output
+
+global_model = ConvNN
+
+def train_epoch(dataloader, model, loss_fn, optimizer, isDebug=False):
+    if not isDebug:
+        size = len(dataloader.dataset) // session.get_world_size()
+    else:
+        size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X = X.to(device)
+        y = y.to(device)
+        # Compute prediction error
+        pred = model(X.float())
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+from sklearn.metrics import confusion_matrix as conf_mat
+def validate_epoch(dataloader, model, loss_fn, Nclasses, isDebug=False):
+    if not isDebug:
+        size = len(dataloader.dataset) // session.get_world_size()
+    else:
+        size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    conf_matrix = np.zeros((Nclasses, Nclasses))
+    with torch.no_grad():
+        for X, y in dataloader:
+            X = X.to(device)
+            y = y.to(device)
+            pred = model(X.float())
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            y_cpu = y.to('cpu')
+            pred_cpu = pred.to('cpu')
+            conf_matrix += conf_mat(y_cpu, pred_cpuq.argmax(1), labels=list(range(Nclasses)))
+    test_loss /= num_batches
+    correct /= size
+    print(
+        f"Test Error: \n "
+        f"Accuracy: {(100 * correct):>0.1f}%, "
+        f"Avg loss: {test_loss:>8f} \n"
+    )
+
+    return test_loss, conf_matrix
+
+def train_func(config: Dict):
+    batch_size = config["batch_size"]
+    lr = config["lr"]
+    epochs = config["epochs"]
+    Nclass = config["Nclass"]
+    isDebug = config['isDebug']
+    slice_len = config['slice_len']
+    num_feats = config['num_feats']
+    num_channels = config['num_chans']
+    device = config['device']
+
+    if isDebug:
+        worker_batch_size = batch_size
+    else:
+        worker_batch_size = batch_size // session.get_world_size()
+
+    # Create data loaders.
+    train_dataloader = DataLoader(ds_train, batch_size=worker_batch_size, shuffle=True)
+    test_dataloader = DataLoader(ds_test, batch_size=worker_batch_size, shuffle=True)
+
+    if not isDebug:
+        train_dataloader = train.torch.prepare_data_loader(train_dataloader)
+        test_dataloader = train.torch.prepare_data_loader(test_dataloader)
+
+    # Create model.
+    model = global_model(classes=Nclass, numChannels=num_channels, slice_len=slice_len, num_feats=num_feats)
+    if not isDebug:
+        model = train.torch.prepare_model(model)
+    else:
+        model.to(device)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+    scheduler = ReduceLROnPlateau(optimizer, 'min', min_lr=0.00001, verbose=True)
+    loss_results = []
+    best_loss = np.inf
+    for e in range(epochs):
+        train_epoch(train_dataloader, model, loss_fn, optimizer, isDebug)
+        loss, conf_matrix = validate_epoch(test_dataloader, model, loss_fn, Nclasses=Nclass, isDebug=isDebug)
+        if best_loss > loss:
+            best_loss = loss
+            pickle.dump(conf_matrix, open('conf_matrix.best.pkl', 'wb'))
+            torch.save({
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+            }, 'model.best.pt')
+        scheduler.step(loss)
+        loss_results.append(loss)
+        if not isDebug:
+            # store checkpoint only if the loss has improved
+            state_dict = model.state_dict()
+            consume_prefix_in_state_dict_if_present(state_dict, "module.")
+            checkpoint = Checkpoint.from_dict(
+                dict(epoch=e, model_weights=state_dict)
+            )
+
+            session.report(dict(loss=loss), checkpoint=checkpoint)
+
+    # return required for backwards compatibility with the old API
+    # TODO(team-ml) clean up and remove return
+    return loss_results
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--isDebug", action='store_true', default=False, help="Run in debug mode")
+    parser.add_argument("--num-workers", "-n", type=int, default=2, help="Sets number of workers for training.")
+    parser.add_argument("--use-gpu", action="store_true", default=False, help="Enables GPU training")
+    parser.add_argument("--address", required=False, type=str, help="the address to use for Ray")
+    parser.add_argument("--test", action="store_true", default=False, help="Testing the model")
+    parser.add_argument("--cp_path", help='Path to the checkpoint to load at test time.')
+    args, _ = parser.parse_known_args()
+
+    protocols = ['802_11ax', '802_11b', '802_11n', '802_11g']
+    ds_train = DSTLDataset(protocols, ds_type='train', slice_len=128, slice_overlap_ratio=0.5, override_gen_map=True)
+    ds_test = DSTLDataset(protocols, ds_type='test', slice_len=128, slice_overlap_ratio=0.5, override_gen_map=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_config = {"lr": 1e-3, "batch_size": 512, "epochs": 5}
+    ds_info = ds_train.info()
+    Nclass = ds_info['nclasses']
+    train_config['Nclass'] = Nclass
+    train_config['isDebug'] = args.isDebug
+    train_config['slice_len'] = ds_info['slice_len']
+    train_config['num_feats'] = 1
+    train_config['num_chans'] = ds_info['nchans']
+    train_config['device'] = device
+
+    """
+    if not train_config['isDebug']:
+        import ray
+
+        ray.init(address=args.address)
+        trainer = TorchTrainer(
+            train_func,
+            train_loop_config=train_config,
+            scaling_config=ScalingConfig(num_workers=args.num_workers, use_gpu=args.use_gpu),
+        )
+        result = trainer.fit()
+        print(f"Results: {result.metrics}")
+    else:
+        train_func(train_config)
+    """
+
+    train_func(train_config)
+
