e69df08addb46329cadecf651d555bd720c1986b
Mauro Belgiovine <mbelgiovine89@gmail.com>
2023-01-24 18:43:52 -0500

    Moved DSTL data generator in the preprocessing folder. Refactored code in training script and separated model definition into a different file. Started working on testing script.

--- cnn_baseline/DSTL_torch_train.py
+++ cnn_baseline/DSTL_torch_train.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from DSTL_dataset import DSTLDataset
+from dstl.preprocessing.DSTL_dataset import DSTLDataset
 from ray.air import session, Checkpoint
 from typing import Dict
 import torch
@@ -12,60 +12,7 @@ import ray.train as train
 import pickle
 from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present
 
-
-# Define model
-class ConvNN(nn.Module):
-    def __init__(self, numChannels=1, slice_len=4, num_feats=18, classes=3):
-        super(ConvNN, self).__init__()
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        self.numChannels = numChannels
-
-        # initialize first set of CONV => RELU => POOL layers
-        self.conv1 = nn.Conv1d(in_channels=numChannels, out_channels=64, kernel_size=7)
-        self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool1d(kernel_size=2, stride=2)
-        ##  initialize second set of CONV => RELU => POOL layers
-        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128,
-                            kernel_size=3)
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool1d(kernel_size=2, stride=2)
-        ## initialize first (and only) set of FC => RELU layers
-
-        # pass a random input
-        rand_x = torch.Tensor(np.random.random((1, numChannels, slice_len)))
-        output_size = torch.flatten(self.maxpool2(self.conv2(self.maxpool1(self.conv1(rand_x))))).shape
-        self.fc1 = nn.Linear(in_features=output_size.numel(), out_features=256)
-        self.relu3 = nn.ReLU()
-        # initialize our softmax classifier
-        self.fc2 = nn.Linear(in_features=256, out_features=classes)
-        self.logSoftmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, x):
-        # x = x.reshape((x.shape[0], self.numChannels, x.shape[2]))   # CNN 1D expects a [N, Cin, L] size of data
-        # pass the input through our first set of CONV => RELU =>
-        # POOL layers
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-        ## pass the output from the previous layer through the second
-        ## set of CONV => RELU => POOL layers
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        ## flatten the output from the previous layer and pass it
-        ## through our only set of FC => RELU layers
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = self.relu3(x)
-        # pass the output to our softmax classifier to get our output
-        # predictions
-        x = self.fc2(x)
-        output = self.logSoftmax(x)
-        # return the output predictions
-        return output
-
-global_model = ConvNN
+from model_cnn1d import Baseline_CNN1D
 
 def train_epoch(dataloader, model, loss_fn, optimizer, use_ray=False):
     if use_ray:
@@ -120,6 +67,7 @@ def validate_epoch(dataloader, model, loss_fn, Nclasses, use_ray=False):
     return test_loss, conf_matrix
 
 def train_func(config: Dict):
+    global_model = config['pytorch_model']
     batch_size = config["batch_size"]
     lr = config["lr"]
     epochs = config["epochs"]
@@ -145,7 +93,7 @@ def train_func(config: Dict):
         test_dataloader = train.torch.prepare_data_loader(test_dataloader)
 
     # Create model.
-    model = global_model(classes=Nclass, numChannels=num_channels, slice_len=slice_len, num_feats=num_feats)
+    model = global_model(classes=Nclass, numChannels=num_channels, slice_len=slice_len)
     if use_ray:
         model = train.torch.prepare_model(model)
     else:
@@ -215,6 +163,7 @@ if __name__ == "__main__":
     train_config = {"lr": 1e-3, "batch_size": 512, "epochs": 5}
     ds_info = ds_train.info()
     Nclass = ds_info['nclasses']
+    train_config['pytorch_model'] = Baseline_CNN1D
     train_config['Nclass'] = Nclass
     train_config['useRay'] = args.useRay    # TODO: fix this, currently it's not working with Ray because the dataset gets replicated among workers
     train_config['slice_len'] = ds_info['slice_len']
