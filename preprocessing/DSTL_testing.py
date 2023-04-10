5c7bb206a444dc9e66cd6ae6ea99d4a9abadd7d3
mqsirera <msireraperello@gmail.com>
2023-04-10 14:09:48 -0400

    Add specific channel specific dBs testing

--- preprocessing/DSTL_testing.py
+++ preprocessing/DSTL_testing.py
@@ -110,7 +110,8 @@ def validate(model, class_map, seq_len, sli_len, channel, cnn=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--one_model", action='store_true', default=False, help="Use one model for all channels")
+    parser.add_argument("--experiment", default='3', choices=['1', '2', '3'], help="Decide which models to test, 1 is for specific model per \
+                        noise and channel, 2 is for specific model per channel and 3 is for single model for all channel and noise conditions")
     parser.add_argument("--use_gpu", action='store_true', default=False, help="Use gpu for inference")
     args, _ = parser.parse_known_args()
     y_trans_lg, y_trans_sm, y_cnn = [], [], []
@@ -118,34 +119,35 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() and args.use_gpu else "cpu")
 
-    if args.one_model:
-        # Load the three models only one time
-        model_lg = TransformerModel(classes=len(PROTOCOLS), d_model=128*2, seq_len=64, nlayers=2, use_pos=False)
-        model_lg.load_state_dict(torch.load(f"{TRANS_PATH}/modelrandom_lg.pt", map_location=device)['model_state_dict'])
-        model_lg.eval()
-        model_sm = TransformerModel(classes=len(PROTOCOLS), d_model=64*2, seq_len=24, nlayers=2, use_pos=False)
-        model_sm.load_state_dict(torch.load(f"{TRANS_PATH}/modelrandom_sm.pt", map_location=device)['model_state_dict'])
-        model_sm.eval()
-        cnn = Baseline_CNN1D(classes=len(PROTOCOLS), numChannels=2, slice_len=512)
-        cnn.load_state_dict(torch.load(f"{CNN_PATH}/model.cnn.random.pt", map_location=device)['model_state_dict'])
-        cnn.eval()
-        if args.use_gpu:
-            model_lg.cuda()
-            model_sm.cuda()
-            cnn.cuda()
-
+    if args.experiment == '1': # Evaluate the models trained for a specific noise and channel condition - we took 10.0 dBs as fixed noise during training
         for channel in CHANNELS:
-            y_trans_lg.append(validate(model_lg, class_map, seq_len=64, sli_len=128, channel=channel))
-            print(f'Accuracy values for channel {channel} and large architecture are: ', y_trans_lg[-1])
-            y_trans_sm.append(validate(model_sm, class_map, seq_len=24, sli_len=64, channel=channel))
-            print(f'Accuracy values for channel {channel} and small architecture are: ', y_trans_sm[-1])
-            y_cnn.append(validate(cnn, class_map, seq_len=1, sli_len=512, channel=channel, cnn=True))
-            print(f'Accuracy values for channel {channel} and cnn architecture are: ', y_cnn[-1])
-        
-        with open('test_results_uniformdist_onemodel.txt', 'w') as f:
-            f.write(str(y_trans_lg) + '%' + str(y_trans_sm) + '%' + str(y_cnn))
+            # Load the three models for each channel evaluation
+            model_lg = TransformerModel(classes=len(PROTOCOLS), d_model=128*2, seq_len=64, nlayers=2, use_pos=False)
+            model_lg.load_state_dict(torch.load(f"{TRANS_PATH}/model{channel}_10_lg.pt", map_location=device)['model_state_dict'])
+            model_lg.eval()
+            model_sm = TransformerModel(classes=len(PROTOCOLS), d_model=64*2, seq_len=24, nlayers=2, use_pos=False)
+            model_sm.load_state_dict(torch.load(f"{TRANS_PATH}/model{channel}_10_sm.pt", map_location=device)['model_state_dict'])
+            model_sm.eval()
+            cnn = Baseline_CNN1D(classes=len(PROTOCOLS), numChannels=2, slice_len=512)
+            cnn.load_state_dict(torch.load(f"{CNN_PATH}/model.cnn.{channel}.10.pt", map_location=device)['model_state_dict'])
+            cnn.eval()
+            if args.use_gpu:
+                model_lg.cuda()
+                model_sm.cuda()
+                cnn.cuda()
 
-    else:
+            for test_channel in CHANNELS:
+                y_trans_lg.append(validate(model_lg, class_map, seq_len=64, sli_len=128, channel=test_channel))
+                print(f'Accuracy values for channel {test_channel} and large architecture are: ', y_trans_lg[-1])
+                y_trans_sm.append(validate(model_sm, class_map, seq_len=24, sli_len=64, channel=test_channel))
+                print(f'Accuracy values for channel {test_channel} and small architecture are: ', y_trans_sm[-1])
+                y_cnn.append(validate(cnn, class_map, seq_len=1, sli_len=512, channel=test_channel, cnn=True))
+                print(f'Accuracy values for channel {test_channel} and cnn architecture are: ', y_cnn[-1])
+        
+            with open(f'test_results_10dBs_{channel}.txt', 'w') as f:
+                f.write(str(y_trans_lg) + '%' + str(y_trans_sm) + '%' + str(y_cnn))
+
+    elif args.experiment == '2': # Evaluate the models trained for a specific channel condition and variable noise conditions 
         for channel in CHANNELS:
             # Load the three models for each channel evaluation
             model_lg = TransformerModel(classes=len(PROTOCOLS), d_model=128*2, seq_len=64, nlayers=2, use_pos=False)
@@ -172,6 +174,33 @@ if __name__ == "__main__":
         
             with open(f'test_results_uniformdist_{channel}.txt', 'w') as f:
                 f.write(str(y_trans_lg) + '%' + str(y_trans_sm) + '%' + str(y_cnn))
+    
+    else: # Experiment 3: # Evaluate the models trained for general noise and channel conditions
+        # Load the three models only one time
+        model_lg = TransformerModel(classes=len(PROTOCOLS), d_model=128*2, seq_len=64, nlayers=2, use_pos=False)
+        model_lg.load_state_dict(torch.load(f"{TRANS_PATH}/modelrandom_lg.pt", map_location=device)['model_state_dict'])
+        model_lg.eval()
+        model_sm = TransformerModel(classes=len(PROTOCOLS), d_model=64*2, seq_len=24, nlayers=2, use_pos=False)
+        model_sm.load_state_dict(torch.load(f"{TRANS_PATH}/modelrandom_sm.pt", map_location=device)['model_state_dict'])
+        model_sm.eval()
+        cnn = Baseline_CNN1D(classes=len(PROTOCOLS), numChannels=2, slice_len=512)
+        cnn.load_state_dict(torch.load(f"{CNN_PATH}/model.cnn.random.pt", map_location=device)['model_state_dict'])
+        cnn.eval()
+        if args.use_gpu:
+            model_lg.cuda()
+            model_sm.cuda()
+            cnn.cuda()
+
+        for channel in CHANNELS:
+            y_trans_lg.append(validate(model_lg, class_map, seq_len=64, sli_len=128, channel=channel))
+            print(f'Accuracy values for channel {channel} and large architecture are: ', y_trans_lg[-1])
+            y_trans_sm.append(validate(model_sm, class_map, seq_len=24, sli_len=64, channel=channel))
+            print(f'Accuracy values for channel {channel} and small architecture are: ', y_trans_sm[-1])
+            y_cnn.append(validate(cnn, class_map, seq_len=1, sli_len=512, channel=channel, cnn=True))
+            print(f'Accuracy values for channel {channel} and cnn architecture are: ', y_cnn[-1])
+        
+        with open('test_results_uniformdist_onemodel.txt', 'w') as f:
+            f.write(str(y_trans_lg) + '%' + str(y_trans_sm) + '%' + str(y_cnn))
     
     fig, ax = plt.subplots(2, 2, figsize = (12, 6))
 
