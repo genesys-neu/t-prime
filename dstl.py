8ed80f981760da55bf8189bbf5645e2e0fc0dd3b
Joshua Groen <josh.groen@gmail.com>
2023-04-19 17:15:31 -0400

    modified:   dstl.py

--- dstl.py
+++ dstl.py
@@ -92,10 +92,10 @@ def machinelearning(exitFlag):
 
 
 if __name__ == '__main__':
-    rec = threading.Thread(target=receiver)
+    rec = threading.Thread(target=receiver, kwargs={freq=freq, N=N, exitFlag=exitFlag})
     rec.start()
 
-    ml = threading.Thread(target=machinelearning)
+    ml = threading.Thread(target=machinelearning, kwargs={exitFlag=exitFlag})
     ml.start()
 
     # gracefully end program
