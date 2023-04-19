b5e6e77505188331605d1fb203ab86590b0fac5a
Joshua Groen <josh.groen@gmail.com>
2023-04-19 17:00:33 -0400

    modified:   dstl.py

--- dstl.py
+++ dstl.py
@@ -41,6 +41,7 @@ def receiver(freq, N):
     rx_stream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CS16, [rx_chan])  # Setup data stream
     sdr.activateStream(rx_stream)  # this turns the radio on
     file_cntr = 0
+    restart_cntr = 0
 
     while not exitFlag:
         sr = sdr.readStream(rx_stream, [rx_buff], N, timeoutUs=timeout_us)
@@ -53,12 +54,14 @@ def receiver(freq, N):
             sdr.activateStream(rx_stream)  # turn on the stream again
             t2 = time.perf_counter()
             print('restarting the stream took {} s'.format(t2 - t1))
+            restart_cntr = restart_cntr + 1
         if not q.full():
             q.put(rx_buff)
             print('Putting ' + str(rx_buff) + ' : ' + str(q.qsize()) + ' items in queue')
 
     sdr.deactivateStream(rx_stream)
     sdr.closeStream(rx_stream)
+    print('Restarted {} times'.format(restart_cntr))
 
 
 def machinelearning():
@@ -66,6 +69,25 @@ def machinelearning():
         if not q.empty():
             item = q.get()
             print('Getting ' + str(item) + ' : ' + str(q.qsize()) + ' items in queue')
+
+            ############################################################################################
+            # Process Signal
+            ############################################################################################
+            # Convert interleaved shorts (received signal) to numpy.complex64 normalized between [-1, 1]
+            s0 = item.astype(float) / np.power(2.0, rx_bits - 1)
+            # print('s0 {}'.format(s0.size))
+            s = (s0[::2] + 1j * s0[1::2])
+            # print('s {}'.format(s.size))
+
+            # Low-Pass Filter
+            taps = firwin(numtaps=101, cutoff=10e6, fs=fs)
+            lpf_samples = np.convolve(s, taps, 'valid')
+
+            # rational resample
+            # Resample to 20e6
+            resampled_samples = resample_poly(lpf_samples, 16, 25)
+            # 16*31.25=500,20*25=500(need LCM because input needs to be an int).
+            # So we go up by factor of 16, then down by factor of 25 to reach final samp_rate of 20e6
 
 
 if __name__ == '__main__':
@@ -75,8 +97,10 @@ if __name__ == '__main__':
     ml = threading.Thread(target=machinelearning)
     ml.start()
 
-    time.sleep(180)
-    exitFlag = 1
+    # gracefully end program
+    # TODO - how do we gracefully kill this?
+    time.sleep(120)
+    exitFlag = 1 # this doesn't seem to work
 
     rec.join()
     ml.join()
