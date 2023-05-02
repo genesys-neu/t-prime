ed3af6838e5a9ed39ea5de061f06db701c67a9df
mqsirera <msireraperello@gmail.com>
2023-05-02 14:08:44 -0400

    writer.py update

--- writer.py
+++ writer.py
@@ -4,6 +4,7 @@ import numpy as np
 while True:
     p = np.random.randint(4)
     for i in range(10):
-        with open('output.txt', 'a') as file:
+        with open('output.txt', 'a') as file:            
             file.write(f'{p}\n')
+        print(p)
         time.sleep(1)
