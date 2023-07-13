6baee7ba3a23385434f263497323d01422ebbddc
Mauro Belgiovine <mbelgiovine89@gmail.com>
2023-07-13 17:41:09 -0400

    Better script to compute spectrograms of OTA signals.

--- preprocessing/gist/npfile2mat.py
+++ preprocessing/gist/npfile2mat.py
@@ -15,18 +15,24 @@ if __name__ == "__main__":
     parser.add_argument('--dbm', nargs='+', default=['0', '10', '20', '30'])
     parser.add_argument('--raw_path', default='/home/miquelsirera/Desktop/dstl/data/RM_573C_power/', help='Path where raw signals are stored.')
     parser.add_argument('--postfix', default='', help='Postfix to append to dataset file.')
+    parser.add_argument('--numfiles', default=10, help='Max num of file per folder to be extracted')
     args, _ = parser.parse_known_args()
 
     os.makedirs(os.path.join(args.raw_path, 'mat'), exist_ok=True)
     for p in args.protocols:
         for dbm in args.dbm:
-            path = os.path.join(args.raw_path, p+'_'+dbm)
-            for f in glob(os.path.join(path, '*.bin')):
-                s = np.fromfile(f, dtype=np.complex128)
-                mat_path = os.path.join(args.raw_path, 'mat', p + '_' + dbm)
-                if not os.path.isdir(mat_path):
-                    os.makedirs(mat_path)
-                sio.savemat(os.path.join(args.raw_path, 'mat', p+'_'+dbm, os.path.basename(f)[:-4]+'.mat'), {'signal': s})
+            folder_name = p+'_'+dbm if dbm != "" else p
+            path = os.path.join(args.raw_path, folder_name)
+
+            for ix, f in enumerate(glob(os.path.join(path, '*.bin'))):
+                if ix < args.numfiles:
+                    s = np.fromfile(f, dtype=np.complex128)
+                    mat_path = os.path.join(args.raw_path, 'mat', folder_name)
+                    if not os.path.isdir(mat_path):
+                        os.makedirs(mat_path)
+                    sio.savemat(os.path.join(args.raw_path, 'mat', folder_name, os.path.basename(f)[:-4]+'.mat'), {'signal': s})
+                else:
+                    break
 
 
 
--- preprocessing/gist/npfile2spectrogram.py
+++ preprocessing/gist/npfile2spectrogram.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--protocols", nargs='+', default=['802.11ax', '802.11b', '802.11n', '802.11g'],
                         help="Specify the protocols/classes to be included in the dataset")
+    parser.add_argument("--fs", required=True, type=float, choices=[20e6, 62.5e6])
+    parser.add_argument("--fc", required=True, type=float, choices=[2.443e9, 2.45e9])
     parser.add_argument('--dbm', nargs='+', default=['0', '10', '20', '30'])
     parser.add_argument('--raw_path', default='/home/miquelsirera/Desktop/dstl/data/RM_573C_power/', help='Path where raw signals are stored.')
     parser.add_argument('--postfix', default='', help='Postfix to append to dataset file.')
@@ -28,17 +30,18 @@ if __name__ == "__main__":
             for ix, bin in enumerate(glob(os.path.join(path, '*.bin'))):
                 if ix < 3:
                     s = np.fromfile(bin, dtype=np.complex128)
-                    fs = 62.5e6 #20e6 we use a wider bandwidth here to collect data
-                    f, t, Sxx = signal.spectrogram(s, fs=fs, nfft=1024)
+                    fs = args.fs
+
+                    f, t, Sxx = signal.spectrogram(s, fs=fs)
                     th = Sxx.max() - 1e-10
                     Sxx[np.where(Sxx > th)] = th    # clip values for visualization po
-                    """
-                    plt.pcolormesh(t, f, Sxx, shading='gouraud')
-                    plt.xlabel('Time [sec]')
-                    plt.ylabel('Frequency [Hz]')
-                    """
-                    plt.pcolormesh(f, t, Sxx.T, shading='auto', cmap='seismic')
-                    fc = 2.45e9
+
+                    plt.specgram(s, Fs=fs)
+                    plt.xlabel("Time")
+                    plt.ylabel("Frequency")
+
+                    #plt.pcolormesh(f, t, Sxx.T, shading='auto', cmap='seismic')
+                    fc = args.fc
                     ch7 = 2.443e9 - fc
                     ch8 = 2.447e9 - fc
                     ch9 = 2.452e9 - fc
@@ -52,30 +55,27 @@ if __name__ == "__main__":
                         ch7_col = 'yellow'
                         ch9_col = 'yellow'
 
-
-                    plt.ylabel('Time [sec]')
-                    plt.xlabel('Frequency [Hz]')
                     plt.title('Protocols: '+str(p)+' '+os.path.basename(args.raw_path))
                     plt.colorbar()
                     #plt.show()
                     plt.savefig(os.path.join(path, p+'_'+os.path.basename(args.raw_path)+'_'+str(ix)+'.png'))
 
-                    plt.vlines(x=0, ymin=0, ymax=max(t),
+                    plt.hlines(y=0, xmin=0, xmax=max(t),
                                colors='red',
                                label='fc = 2.45e9')
 
-                    plt.vlines(x=ch7, ymin=0, ymax=max(t),
+                    plt.hlines(y=ch7, xmin=0, xmax=max(t),
                                colors=ch7_col, linestyles='dashed',
                                label='fc = 2.443e9 [Ch 7]')
 
-                    plt.vlines(x=ch8, ymin=0, ymax=max(t),
+                    plt.hlines(y=ch8, xmin=0, xmax=max(t),
                                colors=ch8_col, linestyles='solid',
                                label='fc = 2.447e9 [Ch 8]')
-                    plt.vlines(x=ch9, ymin=0, ymax=max(t),
+                    plt.hlines(y=ch9, xmin=0, xmax=max(t),
                                colors=ch9_col, linestyles='dashdot',
                                label='fc = 2.452e9[Ch 9]')
 
-                    plt.vlines(x=ch10, ymin=0, ymax=max(t),
+                    plt.hlines(y=ch10, xmin=0, xmax=max(t),
                                colors=ch10_col, linestyles='dotted',
                                label='fc = 2.457e9 [Ch 10]')
 
