2991c8fe80845ec31662c8bad322d15d0be2f378
Mauro Belgiovine <mbelgiovine89@gmail.com>
2023-07-26 19:05:37 -0400

    Fix chan 7 frequency value and fonts on plots

--- preprocessing/gist/npfile2spectrogram.py
+++ preprocessing/gist/npfile2spectrogram.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument("--protocols", nargs='+', default=['802.11ax', '802.11b', '802.11n', '802.11g'],
                         help="Specify the protocols/classes to be included in the dataset")
     parser.add_argument("--fs", required=True, type=float, choices=[20e6, 62.5e6])
-    parser.add_argument("--fc", required=True, type=float, choices=[2.443e9, 2.45e9])
+    parser.add_argument("--fc", required=True, type=float, choices=[2.442e9, 2.45e9])
     parser.add_argument('--dbm', nargs='+', default=['0', '10', '20', '30'])
     parser.add_argument('--raw_path', default='/home/miquelsirera/Desktop/dstl/data/RM_573C_power/', help='Path where raw signals are stored.')
     parser.add_argument('--postfix', default='', help='Postfix to append to dataset file.')
@@ -24,7 +24,8 @@ if __name__ == "__main__":
     import matplotlib.colors as colors
 
     max_plots = 20
-
+    plt.rcParams.update({'font.size': 17})
+    plt.rcParams['figure.figsize'] = [7.5, 5.5]
     for p in args.protocols:
         for dbm in args.dbm:
             filename = p + '_' + dbm if dbm != "" else p
@@ -44,7 +45,7 @@ if __name__ == "__main__":
 
                     #plt.pcolormesh(f, t, Sxx.T, shading='auto', cmap='seismic')
                     fc = args.fc
-                    ch7 = 2.443e9 - fc
+                    ch7 = 2.442e9 - fc
                     ch8 = 2.447e9 - fc
                     ch9 = 2.452e9 - fc
                     ch10 = 2.457e9 - fc
@@ -57,7 +58,8 @@ if __name__ == "__main__":
                         ch7_col = 'yellow'
                         ch9_col = 'yellow'
 
-                    plt.title('Protocols: '+str(p)+' '+os.path.basename(args.raw_path))
+                    #plt.title('Protocols: '+str(p)+' '+os.path.basename(args.raw_path))
+
                     plt.colorbar()
                     #plt.show()
                     plt.savefig(os.path.join(path, p+'_'+os.path.basename(args.raw_path)+'_'+str(ix)+'.png'))
@@ -65,12 +67,12 @@ if __name__ == "__main__":
                     xmin = 1e-5
                     plt.hlines(y=0, xmin=xmin, xmax=max(t),
                                colors='red',
-                               label='fc = '+str(fc))
+                               label='fc = '+"{:e}".format(fc))
 
                     if ch7 != 0:
                         plt.hlines(y=ch7, xmin=xmin, xmax=max(t),
                                    colors=ch7_col, linestyles='dashed',
-                                   label='fc = 2.443e9 [Ch 7]')
+                                   label='fc = 2.442e9 [Ch 7]')
 
                     plt.hlines(y=ch8, xmin=xmin, xmax=max(t),
                                colors=ch8_col, linestyles='solid',
@@ -84,7 +86,8 @@ if __name__ == "__main__":
                                    label='fc = 2.457e9 [Ch 10]')
 
 
-                    plt.legend()
+                    plt.legend(prop={'size': 12})
+
                     #plt.show()
                     plt.savefig(os.path.join(path, p + '_' + os.path.basename(args.raw_path) + '_' + str(ix) + '__lines.png'))
                     plt.clf()
